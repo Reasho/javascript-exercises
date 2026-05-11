0df730d0169f1b3df323ce09d5085fd43c64bb2b
Reasho <106495283+Reasho@users.noreply.github.com>
2026-05-11 19:38:21 -0400

    Finished exercises 10 and 11

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,5 +1,16 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    const four = year % 4 === 0;
+    const century = year % 100 === 0;
+    const fourHundred = year % 400 === 0;
 
+  if (
+    four &&
+    (!century || fourHundred)
+  ) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let degree = (temp - 32) * 5/9;
+  let celcius = Math.round(degree * 10) / 10;
+  return celcius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let degree = (temp * 9/5) + 32;
+  let faren = Math.round(degree * 10) / 10;
+  return faren;
 };
 
 // Do not edit below this line
